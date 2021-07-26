20763b7380d81339480bfe9246ecab7e5f8d5a29
prayogayogi <yogikpiter@gmail.com>
2021-07-27 01:22:12 +0700

    memngurangi file yang tidak penting

--- application/views/pages/auth/login.php
+++ application/views/pages/auth/login.php
@@ -54,7 +54,7 @@
   </div>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <script src="<?= base_url('assets') ?>/login/scripts.js"></script>
+  <!-- <script src="<?= base_url('assets') ?>/login/scripts.js"></script> -->
 </body>
 
 </html>
